3356ffab6b55d572bde4096b1618b32119eb412e
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-10-12 21:12:48 -0600

    Created google calendar API and styled buttons

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
   const [status, setStatus] = useState('all');
   const [filteredTasks, setFilteredTasks] = useState([]);
 
+  var gapi = window.gapi
+  var CLIENT_ID = "831076834116-l2u8v917pgk0lehmumj0qfcldvfoebd8.apps.googleusercontent.com";
+  var API_KEY = "AIzaSyDUBBgjpjKYmvZR2F7gIGByPQ1W3PPZpeo";
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
   useEffect(() => {
     getLocalTasks()
   }, []);
@@ -31,6 +37,66 @@ function App() {
     filterHandler();
     saveLocalTasks();
   }, [tasks, status]);
+
+
+  const handleClick = () => {
+    gapi.load('client:auth2', () => {
+      console.log('Loaded Client')
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('Boom!'))
+
+      gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+        
+        var event = {
+          'summary': 'Awesome Event!',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'Really great refreshments',
+          'start': {
+            'dateTime': '2020-06-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'end': {
+            'dateTime': '2020-06-28T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ],
+          'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'}
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+
+        var request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
+        })
+
+        request.execute(event => {
+          console.log(event)
+          window.open(event.htmlLink)
+        })
+
+      })
+    })
+  }
+
 
   // Save to LocalStorage
   const saveLocalTasks = () => {
@@ -65,6 +131,7 @@ function App() {
       tasks={tasks}
       />
 
+      <button onClick={handleClick} className="addEventButton">Add Event</button>
     </div>
   );
 }
